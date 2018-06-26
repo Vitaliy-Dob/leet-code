c74f7eaa3f4827695ae213783489c64231629e45
Vitalii Dobrotskyi <vdobrotsky@gmail.com>
2018-06-26 18:00:07 +0300

    100. Same Tree

---
+++ LeetCode/LeetCode.Tests/Easy/IsSameTreeTests.cs
@@ -0,0 +1,66 @@
+﻿using Common;
+using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Tests.Easy
+{
+    public class IsSameTreeTests
+    {
+        [Fact]
+        public void IsSameTree_TreesAreTheSame_ShouldReturnTrue()
+        {
+            var tree = new TreeNode(5);
+            tree.left = new TreeNode(3);
+            tree.right = new TreeNode(9);
+            var second_tree = new TreeNode(5);
+            second_tree.left = new TreeNode(3);
+            second_tree.right = new TreeNode(9);
+
+            var result = IsSameTreeImplementation.IsSameTree(tree, second_tree);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsSameTree_OneTreeIsNull_ShouldReturnFalse()
+        {
+            var tree = new TreeNode(5);
+            tree.left = new TreeNode(3);
+            tree.right = new TreeNode(9);
+            TreeNode second_tree = null;            
+
+            var result = IsSameTreeImplementation.IsSameTree(tree, second_tree);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsSameTree_BothNull_ShouldReturnTrue()
+        {
+            TreeNode tree = null;
+            TreeNode second_tree = null;
+
+            var result = IsSameTreeImplementation.IsSameTree(tree, second_tree);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsSameTree_TreesAreDifferent_ShouldReturnFalse()
+        {
+            var tree = new TreeNode(5);
+            tree.left = new TreeNode(3);
+            tree.right = new TreeNode(9);
+            var second_tree = new TreeNode(5);
+            second_tree.left = new TreeNode(9);
+            second_tree.right = new TreeNode(3);
+
+            var result = IsSameTreeImplementation.IsSameTree(tree, second_tree);
+
+            Assert.False(result);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/IsSameTreeImplementation.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public static class IsSameTreeImplementation
+    {
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+
+            if (p == null || q == null)
+                return false;
+
+            return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
