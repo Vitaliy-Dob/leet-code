3e5fa0db83a2906a6fc0fdf2d2ab9a0e1f47cac4
Vitalii Dobrotskyi <vdobrotsky@gmail.com>
2018-07-06 13:06:36 +0300

    198. House Robber.  Cleaner solution. Time Limit Exceeded

--- LeetCode/LeetCode/Easy/RobImplementation.cs
+++ LeetCode/LeetCode/Easy/RobImplementation.cs
@@ -8,26 +8,15 @@ namespace LeetCode.Easy
     {
         public static int Rob(int[] nums)
         {
-            return Calculate(nums.Length - 1, nums);
+            return Calculate(0, nums);
         }
 
         private static int Calculate(int n, int[] nums)
         {
-            if (n < 0)
-            {
+            if (n > nums.Length - 1)
                 return 0;
-            }
 
-            if (n == 1)
-            {
-                return Math.Max(nums[0], nums[1]);
-            }
-            if (n == 0)
-            {
-                return nums[0];
-            }
-
-            return Math.Max(nums[n] + Calculate(n - 2, nums), nums[n - 1] + Calculate(n - 3, nums));
+            return Math.Max(nums[n] + Calculate(n + 2, nums), Calculate(n + 1, nums));
         }
     }
 }
