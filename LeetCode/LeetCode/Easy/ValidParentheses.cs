93eb9300cd772ac6eb37b9c9c52c82bc8675da24
Vitalii Dobrotskyi <vdobr@softserveinc.com>
2019-06-13 16:46:09 +0300

    20. Valid Parentheses enhancment

--- LeetCode/LeetCode/Easy/ValidParentheses.cs
+++ LeetCode/LeetCode/Easy/ValidParentheses.cs
@@ -6,45 +6,36 @@ namespace LeetCode.Easy
 {
     public static class ValidParentheses
     {
-        private const string openingChars = "[{(";
-        private static readonly Dictionary<char, char> brackets = new Dictionary<char, char>
-        {
-            {'[', ']' },
-            {'{', '}' },
-            {'(', ')' }
-        };
-
-
         public static bool IsValid(string s)
         {
             if (string.IsNullOrEmpty(s))
                 return true;
 
-            var opened = new Stack<char>();
+            var stack = new Stack<char>();
 
             for (var i = 0; i < s.Length; i++)
             {
-                if (IsOpening(s[i]))
+                if (s[i] == '[')
                 {
-                    opened.Push(s[i]);
+                    stack.Push(']');
+                    continue;
+                }
+                if (s[i] == '{')
+                {
+                    stack.Push('}');
+                    continue;
+                }
+                if (s[i] == '(')
+                {
+                    stack.Push(')');
                     continue;
                 }
 
-                if (opened.Count == 0)
-                    return false;
-
-                var c = opened.Pop();
-
-                if (brackets[c] != s[i])
+                if (stack.Count == 0 || stack.Pop() != s[i])
                     return false;
             }
 
-            return opened.Count == 0;
-        }
-
-        private static bool IsOpening(char c)
-        {
-            return openingChars.IndexOf(c) != -1;
+            return stack.Count == 0;
         }
     }
 }
