a672471f84ab7c38458d2901462680666c2db682
Vitalii Dobrotskyi <vdobr@softserveinc.com>
2019-06-13 16:37:02 +0300

    234. Palindrome Linked List

---
+++ LeetCode/LeetCode.Tests/Easy/IsPalindromeTests.cs
@@ -0,0 +1,48 @@
+﻿using Common;
+using FluentAssertions;
+using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Tests.Easy
+{
+    public class IsPalindromeTests
+    {
+        [Fact]
+        public void IsPalindrome_ReturnsFalse()
+        {
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+
+            var result = IsPalindromeImplementation.IsPalindrome(head);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsPalindrome_ReturnsTrue()
+        {
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(2);
+            head.next.next.next = new ListNode(1);
+
+            var result = IsPalindromeImplementation.IsPalindrome(head);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsPalindrome_ReturnsTrue2()
+        {
+            var head = new ListNode(1);           
+
+            var result = IsPalindromeImplementation.IsPalindrome(head);
+
+            result.Should().BeTrue();
+        }
+
+    }
+}
---
+++ LeetCode/LeetCode/Easy/IsPalindromeImplementation.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public static class IsPalindromeImplementation
+    {
+        public static bool IsPalindrome(ListNode head)
+        {
+            var list = new List<int>();
+
+            while (head != null)
+            {
+                list.Add(head.val);
+                head = head.next;
+            }
+
+            for (var i = 0; i < list.Count / 2; i++)
+            {
+                if (list[i] != list[list.Count - i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
