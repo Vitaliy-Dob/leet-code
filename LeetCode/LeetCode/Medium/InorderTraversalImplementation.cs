7c66b3db05da028e0ccfe56d490b03e5c066fb60
Vitaliy-Dob <vdobrotsky@gmail.com>
2019-07-17 18:14:15 +0300

    94. Binary Tree Inorder Traversal. ugly but correct implementation

--- LeetCode/LeetCode/Medium/InorderTraversalImplementation.cs
+++ LeetCode/LeetCode/Medium/InorderTraversalImplementation.cs
@@ -1,6 +1,7 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace LeetCode.Medium
@@ -10,21 +11,42 @@ namespace LeetCode.Medium
         public static IList<int> InorderTraversal(TreeNode root)
         {
             var result = new List<int>();
-            if (root != null)
-                InOrder(result, root);
+
+            if (root == null)
+                return result;
+
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            bool bottomDirection = true;
+
+            while (stack.Any())
+            {
+                if (bottomDirection)
+                {
+                    var node = stack.Peek();
+                    if (node.left != null)
+                    {
+                        stack.Push(node.left);
+                        bottomDirection = true;
+                        continue;
+                    }
+
+                    bottomDirection = false;
+                    continue;
+                }
+                else
+                {
+                    var node = stack.Pop();
+                    result.Add(node.val);
+                    if (node.right != null)
+                    {
+                        stack.Push(node.right);
+                        bottomDirection = true;
+                    }
+                }
+            }
 
             return result;
-        }
-
-        private static void InOrder(List<int> result, TreeNode root)
-        {
-            if (root.left != null)
-                InOrder(result, root.left);
-
-            result.Add(root.val);
-
-            if (root.right != null)
-                InOrder(result, root.right);
         }
     }
 }
