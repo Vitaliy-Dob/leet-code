5fcb2ca8c15be74abb19eb7b7a9c874901161acb
Vitaliy-Dob <vdobrotsky@gmail.com>
2019-10-04 18:44:24 +0300

    62. Unique Paths. Time exceeded

--- LeetCode/LeetCode/Medium/UniquePathsImplementation.cs
+++ LeetCode/LeetCode/Medium/UniquePathsImplementation.cs
@@ -6,19 +6,17 @@ namespace LeetCode.Medium
         {
             if (m == 0 && n == 0)
                 return 0;
-            var matrix = new int[m, n];
-            Go(matrix, 0, 0);
-            return matrix[m - 1, n - 1];
-        }
+            var matrix = new int[m + 1, n + 1];
+            matrix[0, 1] = 1;
+            for (var i = 1; i < matrix.GetLength(0); i++)
+            {
+                for (var j = 1; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = matrix[i - 1, j] + matrix[i, j - 1];
+                }
+            }
 
-        public static void Go(int[,] matrix, int m, int n)
-        {
-            if (matrix.GetLength(0) - 1 < m || matrix.GetLength(1) - 1 < n)
-                return;
-
-            matrix[m, n] = 1 + matrix[m, n];
-            Go(matrix,m + 1, n);
-            Go(matrix, m, n + 1);
+            return matrix[m, n];
         }
     }
 }
